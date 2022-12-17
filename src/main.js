0d08387e4134c717a017c072ed975f0b54319103
曾逸超 <zengyichao@digibms.com>
2022-12-17 21:58:59 +0800

    feat(element-plus): element-plus 引入

--- src/main.js
+++ src/main.js
@@ -4,8 +4,15 @@ import router from './router'
 import store from './store'
 
 import { installDirectives } from '@/directives/index'
+import installElementPlus from './plugins/element'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
+installElementPlus(app)
 installDirectives(app)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.use(store).use(router).mount('#app')
